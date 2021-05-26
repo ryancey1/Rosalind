b63eee76facc59c64d688d73a3640fb0eaf54e06
Ryan Yancey <ryanjyancey@gmail.com>
2021-05-26 15:15:35 -0700

    Memoized

--- Bioinformatics-Stronghold/04-rabbits-and-recurrence-relations.py
+++ Bioinformatics-Stronghold/04-rabbits-and-recurrence-relations.py
@@ -2,18 +2,32 @@
 
 '''
 Given: Positive integers n≤40 and k≤5.
+
 Return: The total number of rabbit pairs that will be present after n months, if we begin with 1 pair and in each generation, every pair of reproduction-age rabbits produces a litter of k rabbit pairs (instead of only 1 pair).
 '''
 
-n = 5
-k = 3
+import sys
 
-def fib(n, k):
-    if n == 0:
+def fib_memo(month, litter_size, memo):
+    args = (month, litter_size)
+    if args in memo:
+        return memo[args]
+    if month == 1:
         return 1
-    elif n == 1:
-        return k
+    if month == 2:
+        return 1
     else:
-        return fib(n-1, k) + fib(n-2, k)
+        gen1 = fib_memo(month - 1, litter_size, memo)
+        gen2 = (fib_memo(month - 2, litter_size, memo) * litter_size)
+        memo[args] = gen1 + gen2
+        return gen1 + gen2
 
-print(fib(n, k))
+if __name__ == '__main__':
+    # if a file is given
+    file = open(sys.argv[1]).read()
+    nk = file.split()
+    n, k = int(nk[0]), int(nk[1])
+    if n > 40 or k > 5:
+        exit('Either the number of months or offspring number is too large.')
+    # process the input and print the result
+    print(fib_memo(n, k, {}))
